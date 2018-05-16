80768303d22d334cb3a2888278a0772a6c780436
chairco <chairco@gmail.com>
2018-05-16 17:30:16 +0800

    Update theme to nest

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,7 +38,8 @@ MD_EXTENSIONS = [
 
 
 # Theme Path
-THEME = "./pelican-clean-blog/"
+#THEME = "./pelican-clean-blog/"
+THEME = "./nest/"
 
 
 # Theme setting
@@ -80,13 +81,13 @@ AUTHOR_FEED_RSS = None
 
 # Social widget
 SOCIAL = (
-    ('google', '#'),
-    ('twitter', 'https://twitter.com/ChaircoChen'),
-    ('facebook', 'https://www.facebook.com/chairco'),
-    ('github', 'https://github.com/chairco'),
+    ('Google', '#'),
+    ('Twitter', 'https://twitter.com/ChaircoChen'),
+    ('Facebook', 'https://www.facebook.com/chairco'),
+    ('Github', 'https://github.com/chairco'),
     ('RSS', 'http://chairco.me/feeds/all.atom.xml'),
-    ('envelope-o', 'mailto:chairco@gmail.com'),
-    ('linkedin', '#'),
+    ('Email', 'mailto:chairco@gmail.com'),
+    ('Linkedin', '#'),
 )
 
 
@@ -95,6 +96,99 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+
+# NEST Template
+THEME = 'nest'
+SITESUBTITLE = u"Jason's Blog"
+# Minified CSS
+NEST_CSS_MINIFY = True
+# Add items to top menu before pages
+MENUITEMS = [
+    ('About', '/pages/about-me/'),
+    ('Resume', '/cv'),
+    ('Archives', '/archives.html'),
+    ('Categories', '/categories.html'),
+    ('Tags', '/tags.html'),
+    ('RSS', '/feeds/all.atom.xml')
+]
+# Add header background image from content/images : 'background.jpg'
+NEST_HEADER_IMAGES = 'home-bg.jpg'
+NEST_HEADER_LOGO = '/images/logo.png'
+# Footer
+NEST_SITEMAP_COLUMN_TITLE = u'Sitemap'
+NEST_SITEMAP_MENU = [('Archives', '/archives.html'),('Tags','/tags.html'), ('Authors','/authors.html')]
+NEST_SITEMAP_ATOM_LINK = u'Atom Feed'
+NEST_SITEMAP_RSS_LINK = u'RSS Feed'
+NEST_SOCIAL_COLUMN_TITLE = u'Social'
+NEST_LINKS_COLUMN_TITLE = u'Links'
+NEST_COPYRIGHT = u"&copy; Jason's blog 2018"
+# Footer optional
+NEST_FOOTER_HTML = ''
+# index.html
+NEST_INDEX_HEAD_TITLE = u'noname'
+NEST_INDEX_HEADER_TITLE = u'廢廢的阿宅，來看一下嘛，反正不用錢 >///<'
+NEST_INDEX_HEADER_SUBTITLE = u'Taiwan No.1'
+NEST_INDEX_CONTENT_TITLE = u'Last Posts'
+# archives.html
+NEST_ARCHIVES_HEAD_TITLE = u'Archives'
+NEST_ARCHIVES_HEAD_DESCRIPTION = u'Posts Archives'
+NEST_ARCHIVES_HEADER_TITLE = u'Archives'
+NEST_ARCHIVES_HEADER_SUBTITLE = u'Archives for all posts'
+NEST_ARCHIVES_CONTENT_TITLE = u'Archives'
+# article.html
+NEST_ARTICLE_HEADER_BY = u'By'
+NEST_ARTICLE_HEADER_MODIFIED = u'modified'
+NEST_ARTICLE_HEADER_IN = u'in category'
+# author.html
+NEST_AUTHOR_HEAD_TITLE = u'Posts by'
+NEST_AUTHOR_HEAD_DESCRIPTION = u'Posts by'
+NEST_AUTHOR_HEADER_SUBTITLE = u'Posts archives'
+NEST_AUTHOR_CONTENT_TITLE = u'Posts'
+# authors.html
+NEST_AUTHORS_HEAD_TITLE = u'Author list'
+NEST_AUTHORS_HEAD_DESCRIPTION = u'Author list'
+NEST_AUTHORS_HEADER_TITLE = u'Author list'
+NEST_AUTHORS_HEADER_SUBTITLE = u'Archives listed by author'
+# categories.html
+NEST_CATEGORIES_HEAD_TITLE = u'Categories'
+NEST_CATEGORIES_HEAD_DESCRIPTION = u'Archives listed by category'
+NEST_CATEGORIES_HEADER_TITLE = u'Categories'
+NEST_CATEGORIES_HEADER_SUBTITLE = u'Archives listed by category'
+# category.html
+NEST_CATEGORY_HEAD_TITLE = u'Category Archive'
+NEST_CATEGORY_HEAD_DESCRIPTION = u'Category Archive'
+NEST_CATEGORY_HEADER_TITLE = u'Category'
+NEST_CATEGORY_HEADER_SUBTITLE = u'Category Archive'
+# pagination.html
+NEST_PAGINATION_PREVIOUS = u'Previous'
+NEST_PAGINATION_NEXT = u'Next'
+# period_archives.html
+NEST_PERIOD_ARCHIVES_HEAD_TITLE = u'Archives for'
+NEST_PERIOD_ARCHIVES_HEAD_DESCRIPTION = u'Archives for'
+NEST_PERIOD_ARCHIVES_HEADER_TITLE = u'Archives'
+NEST_PERIOD_ARCHIVES_HEADER_SUBTITLE = u'Archives for'
+NEST_PERIOD_ARCHIVES_CONTENT_TITLE = u'Archives for'
+# tag.html
+NEST_TAG_HEAD_TITLE = u'Tag archives'
+NEST_TAG_HEAD_DESCRIPTION = u'Tag archives'
+NEST_TAG_HEADER_TITLE = u'Tag'
+NEST_TAG_HEADER_SUBTITLE = u'Tag archives'
+# tags.html
+NEST_TAGS_HEAD_TITLE = u'Tags'
+NEST_TAGS_HEAD_DESCRIPTION = u'Tags List'
+NEST_TAGS_HEADER_TITLE = u'Tags'
+NEST_TAGS_HEADER_SUBTITLE = u'Tags List'
+NEST_TAGS_CONTENT_TITLE = u'Tags List'
+NEST_TAGS_CONTENT_LIST = u'tagged'
+# Static files
+STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'extra/logo.svg']
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/logo.svg': {'path': 'logo.svg'}
+}
+
 
 # Disqus
 DISQUS_SITENAME = "chairco"
